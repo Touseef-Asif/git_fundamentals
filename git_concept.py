20ad6b6420601342371d33d007f8439da89e8052
Touseef Asif <161280202+Touseef-Asif@users.noreply.github.com>
2026-06-17 23:31:27 -0700

    sigapore update 1

--- git_concept.py
+++ git_concept.py
@@ -6,3 +6,12 @@ b = tested(4)
 print(f'square of given nubmer is {b}')
 
 print("Akhir is marz ki dawa kia hai")
+
+
+print("this is the code from the singapore Machine")
+def greaterNumber(x, y):
+    if x>y:
+        print(f'{x} is greater than {y}')
+    else:
+        print(f'{y} is greater than {x}')
+greaterNumber(3,4)
